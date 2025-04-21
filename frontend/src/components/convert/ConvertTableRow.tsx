4e2c2d173c5715e8073f5aa8b16414b1af226d3d
Jangsojung <wkdthwjd99@naver.com>
2025-04-21 12:52:19 +0900

    Refactor: 데이터 변환 파일 구조 수정

---
+++ frontend/src/components/convert/ConvertTableRow.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui/material/TableCell';
+import Checkbox from '@mui/material/Checkbox';
+
+export default function ConvertTableRow({ base, checked, onCheckboxChange }) {
+  const handleChange = () => {
+    onCheckboxChange(base.ab_idx);
+  };
+
+  return (
+    <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+      <TableCell>
+        <Checkbox checked={checked} onChange={handleChange} />
+      </TableCell>
+      <TableCell>{base.ab_idx}</TableCell>
+      <TableCell>{base.ab_name}</TableCell>
+    </TableRow>
+  );
+}
--- frontend/src/pages/aas/convert.tsx
+++ frontend/src/pages/aas/convert.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
-import Sort from '../../layouts/sort/convert';
-import Convert from '../../section/aas/convert/view';
+import React, { useState } from 'react';
+import { getBasesAPI, insertBaseAPI } from '../../apis/api/convert';
+
 import CircularProgress from '@mui/material/CircularProgress';
+import { Dayjs } from 'dayjs';
+import { Box, Button, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import Grid from '@mui/system/Grid';
+import BasicDatePicker from '../../components/datepicker';
+import Pagenation from '../../components/pagenation';
+import Paper from '@mui/material/Paper';
+import ConvertTableRow from '../../components/convert/ConvertTableRow';
 
 export default function ConvertPage() {
-  const [isLoading, setIsLoading] = React.useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectedConvert, setSelectedConvert] = useState<number | null>();
+  const [bases, setBases] = useState<Base[]>([]);
+
+  const [startDate, setStartDate] = useState<Dayjs | null>(null);
+  const [endDate, setEndDate] = useState<Dayjs | null>(null);
+  const [currentPage, setCurrentPage] = React.useState(0);
+  const rowsPerPage = 10;
+
+  interface Base {
+    ab_idx: number;
+    ab_name: string;
+    sn_length: number;
+  }
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const pagedData = bases.slice(currentPage * rowsPerPage, (currentPage + 1) * rowsPerPage);
 
   const startLoading = () => {
     setIsLoading(true);
@@ -13,6 +39,49 @@ export default function ConvertPage() {
   const endLoading = () => {
     setIsLoading(false);
   };
+
+  const handleDateChange = (newStartDate: Dayjs | null, newEndDate: Dayjs | null) => {
+    setStartDate(newStartDate);
+    setEndDate(newEndDate);
+  };
+
+  const getBases = async () => {
+    const data: Base[] = await getBasesAPI();
+    setBases(data);
+  };
+
+  const handleInsert = async () => {
+    if (!startDate || !endDate) {
+      alert('시작 날짜와 종료 날짜를 모두 선택해야 합니다.');
+      return;
+    }
+
+    startLoading();
+
+    const formattedStartDate = startDate.format('YYMMDD');
+    const formattedEndDate = endDate.format('YYMMDD');
+
+    const data = await insertBaseAPI(formattedStartDate, formattedEndDate, selectedConvert);
+
+    if (data) {
+      alert('성공적으로 json파일을 생성하였습니다.\n파일 위치: /files/front');
+
+      setStartDate(null);
+      setEndDate(null);
+      setSelectedConvert(null);
+    }
+
+    endLoading();
+  };
+
+  const handleCheckboxChange = (convertsIdx: number) => {
+    setSelectedConvert((prev) => (prev === convertsIdx ? null : convertsIdx));
+  };
+
+  React.useEffect(() => {
+    getBases();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div style={{ position: 'relative' }}>
@@ -37,9 +106,67 @@ export default function ConvertPage() {
       )}
 
       <div style={{ pointerEvents: isLoading ? 'none' : 'auto' }}>
-        <Sort startLoading={startLoading} endLoading={endLoading} />
-        <Convert />
+        <Box sx={{ flexGrow: 1 }} className='sort-box'>
+          <Grid container spacing={1}>
+            <Grid size={8}>
+              <Grid container spacing={1}>
+                <Grid size={10}>
+                  <Grid container spacing={1}>
+                    <Grid className='d-flex gap-5'>
+                      <div className='sort-title'>날짜</div>
+                    </Grid>
+                    <Grid>
+                      <BasicDatePicker onDateChange={handleDateChange} startDate={startDate} endDate={endDate} />
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+
+            <Grid size={4}>
+              <Stack spacing={1} direction='row' style={{ justifyContent: 'flex-end' }}>
+                <Button variant='contained' color='success' onClick={handleInsert} disabled={selectedConvert === null}>
+                  등록
+                </Button>
+              </Stack>
+            </Grid>
+          </Grid>
+        </Box>
+        <div className='table-wrap'>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+              <TableHead>
+                <TableRow>
+                  <TableCell></TableCell>
+                  {cells.map((cell, idx) => (
+                    <TableCell key={idx}>{cell}</TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {pagedData.length > 0 ? (
+                  pagedData.map((base, idx) => (
+                    <ConvertTableRow
+                      base={base}
+                      key={idx}
+                      checked={selectedConvert === base.ab_idx}
+                      onCheckboxChange={handleCheckboxChange}
+                    />
+                  ))
+                ) : (
+                  <TableRow>
+                    <TableCell colSpan={cells.length + 1} align='center'>
+                      데이터가 없습니다.
+                    </TableCell>
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <Pagenation count={bases ? bases.length : 0} onPageChange={handlePageChange} />
+        </div>
       </div>
     </div>
   );
 }
+const cells = ['번호', '기초코드 이름'];
