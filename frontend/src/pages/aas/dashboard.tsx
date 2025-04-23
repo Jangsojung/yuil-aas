1b4cc65445ce0f73d8594a88c59d3ba4208c0cb4
Jangsojung <wkdthwjd99@naver.com>
2025-04-23 17:44:54 +0900

    Refactor: 기초코드 reset

--- frontend/src/pages/aas/dashboard.tsx
+++ frontend/src/pages/aas/dashboard.tsx
@@ -1,14 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Sort from '../../layouts/sort/base_code';
 import Sort2 from '../../layouts/sort/basic_code';
 import BasicCode from '../../section/aas/dashboard/view';
 import BaseCode from '../../section/aas/dashboard/view/base_index';
-import { useRecoilValue } from 'recoil';
+import { useRecoilState } from 'recoil';
 import { baseEditModeState } from '../../recoil/atoms';
 
 export default function DashboardPage() {
   const [insertMode, setInsertMode] = useState(false);
-  const baseEditMode = useRecoilValue(baseEditModeState);
+  const [baseEditMode, setBaseEditMode] = useRecoilState(baseEditModeState);
+
+  useEffect(() => {
+    setInsertMode(false);
+    setBaseEditMode(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div>
       {insertMode || baseEditMode ? (
