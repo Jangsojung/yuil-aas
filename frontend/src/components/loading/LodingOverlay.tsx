974f6811c0757276000eed40bade2337be11a009
Jangsojung <wkdthwjd99@naver.com>
2025-06-26 16:36:50 +0900

    Refactor: 색 변경

--- frontend/src/components/loading/LodingOverlay.tsx
+++ frontend/src/components/loading/LodingOverlay.tsx
@@ -9,7 +9,7 @@ export default function LoadingOverlay() {
         left: 0,
         right: 0,
         bottom: 0,
-        backgroundColor: 'rgba(255, 255, 255, 0.7)',
+        backgroundColor: 'rgba(51, 51, 51, 0.7)',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
